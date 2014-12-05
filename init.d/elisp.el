eb7289e35bcd30eec574ec105f85c7381fe8bf63
Aldric Giacomoni <trevoke@gmail.com>
2014-12-04 19:08:27 -0500

    separate emacs from rest of config

---
+++ init.d/elisp.el
@@ -0,0 +1,9 @@
+(add-hook 'lisp-mode-hook 'stag-code-modes-hook)
+(add-hook 'lisp-mode-hook 'paredit-mode)
+
+(add-hook 'emacs-lisp-mode-hook 'stag-code-modes-hook)
+(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
---
+++ init.d/javascript.el
@@ -0,0 +1,2 @@
+(add-to-list 'auto-mode-alist '(".js$" . js3-mode))
+(add-hook 'js3-mode-hook 'stag-code-modes-hook)
