eaf0c25be71ddcc9bc1487d7a34315688df285ec
Aldric Giacomoni <trevoke@gmail.com>
2014-12-05 20:04:59 -0500

    Define functions to open line above/below point

--- init.d/00-additional-functions.el
+++ init.d/00-additional-functions.el
@@ -1,7 +1,7 @@
 (defun what-face (pos)
   (interactive "d")
   (let ((face (or (get-char-property pos 'read-face-name)
-                  (get-char-property pos 'face))))
+		  (get-char-property pos 'face))))
     (if face (message "Face: %s" face) (message "No face at %d" pos))))
 
 (defun stag-code-modes-hook ()
@@ -28,3 +28,20 @@
 
 (global-set-key (kbd "C-x 2") 'vsplit-last-buffer)
 (global-set-key (kbd "C-x 3") 'hsplit-last-buffer)
+
+(defun stag-insert-line-below ()
+  "Insert and auto-indent line below cursor, like in vim."
+  (interactive)
+  (open-line 1)
+  (next-line)
+  (indent-for-tab-command))
+
+(defun stag-insert-line-above ()
+  "Insert and auto-indent line above cursor, like in vim."
+  (interactive)
+  (previous-line)
+  (move-end-of-line 1)
+  (stag-insert-line-below))
+
+(global-set-key (kbd "C-o") 'stag-insert-line-below)
+(global-set-key (kbd "C-M-o") 'stag-insert-line-above)
