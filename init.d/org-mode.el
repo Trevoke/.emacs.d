97287a70eb8051a8e0556803618f6cab670d6ff7
Aldric Giacomoni <trevoke@gmail.com>
2014-12-11 22:02:16 -0500

    Easier orgtbl-to-gfm tables

--- init.d/org-mode.el
+++ init.d/org-mode.el
@@ -6,3 +6,36 @@
 (setq org-startup-indented t)
 
 (setq org-log-done 'time)
+
+;; Usage Example:
+;;
+;; <!-- BEGIN RECEIVE ORGTBL ${1:YOUR_TABLE_NAME} -->
+;; <!-- END RECEIVE ORGTBL $1 -->
+;;
+;; <!--
+;; #+ORGTBL: SEND $1 orgtbl-to-gfm
+;; | $0 |
+;; -->
+
+(defun orgtbl-to-gfm (table params)
+  "Convert the Orgtbl mode TABLE to GitHub Flavored Markdown."
+  (let* ((alignment (mapconcat (lambda (x) (if x "|--:" "|---"))
+			       org-table-last-alignment ""))
+	 (params2
+	  (list
+	   :splice t
+	   :hline (concat alignment "|")
+	   :lstart "| " :lend " |" :sep " | ")))
+    (orgtbl-to-generic table (org-combine-plists params2 params))))
+
+(defun stag-insert-org-to-md-table (table-name)
+  (interactive "*sEnter table name: ")
+  (insert "<!---
+#+ORGTBL: SEND " table-name " orgtbl-to-gfm
+
+-->
+<!--- BEGIN RECEIVE ORGTBL " table-name " -->
+<!--- END RECEIVE ORGTBL " table-name " -->")
+  (previous-line)
+  (previous-line)
+  (previous-line))
