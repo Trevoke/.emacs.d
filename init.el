b3d2c85e5579281c93cf35c0b79416f3a845758f
Aldric Giacomoni <trevoke@gmail.com>
2014-12-20 09:13:05 -0500

    add yasnippet, move to js2

--- init.el
+++ init.el
@@ -66,8 +66,14 @@
 (add-hook 'ag-mode-hook 'wgrep-ag-setup)
 
 ;; guide-key
-(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-h" "C-c" "C-x" "C-x a" "C-x C-a" "C-x 8"))
+(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-h" "C-c"
+				     "C-x" "C-x a" "C-x C-a" "C-x 8"
+				     "C-u" "C-u C-x"
+				     ))
 (guide-key-mode 1)
+
+;; Yasnippet
+(yas-global-mode 1)
 
 ;; blindly copied, not sure how to organize this.
 (eval-after-load 'auto-complete
