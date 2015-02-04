a2704d559a3d48feb8d18e6a81a2c9d4c5848bba
Aldric Giacomoni <trevoke@gmail.com>
2015-02-04 14:25:42 -0500

    Add autocomplete sources

--- init.el
+++ init.el
@@ -4,17 +4,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(custom-safe-themes
    (quote
-    ("18e89f93cbaaac214202142d910582354d36639f21f32b04718ca6425dbc82a2" "cdc7555f0b34ed32eb510be295b6b967526dd8060e5d04ff0dce719af789f8e5" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "0653b54b345b36e4228809f3d95bba0433ee098ae56043bc8b89d78ddf5e4d24" "1f174cdd9e0f26f4068068cf7299ed7d79ce6b2942a89e4da7176ae6ee7768de" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "d08bb22f4092c71bcdc4979c7ec79abc40dc047f5b604bd34996a2ead2ce870b" "18e89f93cbaaac214202142d910582354d36639f21f32b04718ca6425dbc82a2" "cdc7555f0b34ed32eb510be295b6b967526dd8060e5d04ff0dce719af789f8e5" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "0653b54b345b36e4228809f3d95bba0433ee098ae56043bc8b89d78ddf5e4d24" "1f174cdd9e0f26f4068068cf7299ed7d79ce6b2942a89e4da7176ae6ee7768de" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
  '(ggtags-executable-directory "/usr/local/bin")
  '(mu-worlds
    (quote
     (["Discworld" "discworld.starturtle.net" 4242 "Muad" "" "" ""])))
- '(paradox-github-token t t))
+ '(paradox-github-token t t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 140 :family "Monaco")))))
+ '(default ((t (:height 140 :width normal :family "Monaco")))))
