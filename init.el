03ca13d47d96fb8092728fa2eabfdd82f9c04ff1
Aldric Giacomoni <trevoke@gmail.com>
2017-01-20 20:45:46 -0500

    smex/flx, also delete indentation better?

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
-                                        ;(package-initialize)
+; (package-initialize)
 
-(require 'package)
 (setq package-enable-at-startup nil)
+(require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa-stable" . "https://stable.melpa.org/packages/")
@@ -22,18 +22,11 @@
        (assq (car args) package-alist)))
 
 (advice-add 'package-installed-p :around 'package-from-archive)
+(use-package org :pin "org")
+(advice-remove 'package-installed-p 'package-from-archive)
 
 (setq use-package-always-pin "melpa-stable")
 (setq use-package-always-ensure t)
-;; (setq use-package-always-defer t)
-
-;; (add-to-list 'load-path "~/.emacs.d/packages/org/lisp")
-;; (add-to-list 'load-path "~/.emacs.d/packages/org/contrib/lisp" t)
-;; (require 'org)
-;;
-(use-package org :pin "org")
-
-(advice-remove 'package-installed-p 'package-from-archive)
 
 (org-babel-load-file "~/.emacs.d/aldric.org")
 (put 'dired-find-alternate-file 'disabled nil)
